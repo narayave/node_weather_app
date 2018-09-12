a5fd36a2c2686b14c052711d2bd3d427fc0588d8
Vedanth Narayanan <vedanth.nd@gmail.com>
2018-09-12 15:15:18 -0700

    Able to deploy to AWS

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.get('/', function (req, res) {
 	res.render('index', {weather: null, error: null});
 })
 
+var port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port '+  3000 + '!')
 })
 
 app.post('/', function (req, res) {
